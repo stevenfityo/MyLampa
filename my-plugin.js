89a1bd3575dcb35f418d8018e43b626513548fd8
Stepan Fityo <steven.fityo@gmail.com>
2026-06-03 00:02:05 +0300

    v2: minimal load test with startup toast

--- my-plugin.js
+++ my-plugin.js
@@ -1,30 +1,19 @@
-try {
+(function () {
+    Lampa.Noty.show('plugin loaded v2');
+
     Lampa.Listener.follow('full', function (event) {
         if (event.type !== 'complite') return;
 
         setTimeout(function () {
-            try {
-                var candidates = [
-                    '.full-start__buttons',
-                    '.details__buttons',
-                    '.full__buttons',
-                    '.card-full__buttons',
-                    '.full-start',
-                    '.details',
-                    '.full',
-                    '.card-full'
-                ];
+            var found = [];
+            var list = ['.full-start__buttons', '.details__buttons', '.full__buttons',
+                        '.card-full__buttons', '.full-start', '.details', '.full', '.card-full'];
 
-                var found = candidates.filter(function (s) {
-                    return document.querySelector(s);
-                });
-
-                Lampa.Noty.show(found.length ? 'found: ' + found.join(' | ') : 'no selectors found');
-            } catch (e) {
-                Lampa.Noty.show('err: ' + e.message);
+            for (var i = 0; i < list.length; i++) {
+                if (document.querySelector(list[i])) found.push(list[i]);
             }
+
+            Lampa.Noty.show(found.length ? found.join(' | ') : 'no match');
         }, 600);
     });
-} catch (e) {
-    Lampa.Noty.show('outer: ' + e.message);
-}
+})();
